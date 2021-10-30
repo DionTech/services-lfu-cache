709947752375f515360ba0e2cf8a6ed0c5227850
Daniel Koch <daniel.koch@publicare.de>
2021-10-30 22:26:54 +0200

    cache reducing is basically implemented

--- lfu/cache.go
+++ lfu/cache.go
@@ -3,6 +3,7 @@ package lfu
 import (
 	"errors"
 	"runtime"
+	"sort"
 	"sync"
 )
 
@@ -21,12 +22,18 @@ type Cache struct {
 
 var LFU Cache
 
-//Init will init a new cache. Define the max size of the cache in bytes
+// Init will init a new cache. Define the max size of the cache in bytes.
+// Minimum is 500000 (0.5 MB), but this only a theoretic one and not recommended.
+// Minimum is required to can store some elements, there are basic heap allocations caused by maps.
 func Init(size uint64) Cache {
+	if size < 500000 {
+		panic("minimum size is 500000")
+	}
+
 	return Cache{size: size}
 }
 
-//Put will put a cache key to the list.
+// Put will put a cache key to the list.
 func (cache *Cache) Put(key string, item interface{}) (bool, error) {
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
@@ -52,7 +59,7 @@ func (cache *Cache) Put(key string, item interface{}) (bool, error) {
 	return true, nil
 }
 
-//Get will retrieve a cache key from the list.
+// Get will retrieve a cache key from the list.
 func (cache *Cache) Get(key string) (interface{}, error) {
 	cache.lock.RLock()
 	defer cache.lock.RUnlock()
@@ -73,7 +80,7 @@ func (cache *Cache) Get(key string) (interface{}, error) {
 	return item, nil
 }
 
-//Forget will delete a cache key from the list.
+// Forget will delete a cache key from the list.
 func (cache *Cache) Forget(key string) error {
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
@@ -98,7 +105,41 @@ func (cache *Cache) Forget(key string) error {
 	return nil
 }
 
-//calcHeap will store the HeapAlloc bytes at cache.heap
+// Reduce will reduce the map to a max heap size.
+// Our first algorithm here is the LFU one.
+// @TODO: later we can try to optimize it; there will be a few more possibilities (integrating when was the last hit and when was the creation date)
+func (cache *Cache) reduce(max uint64) (bool, error) {
+	//first we will build a slice
+	keys := cache.getSortedTrackingKeys()
+
+	for _, key := range keys {
+		cache.Forget(key)
+		if cache.heap <= max {
+			break
+		}
+	}
+
+	return true, nil
+}
+
+// getSortedTrackingKeys will sort desc by hits
+func (cache *Cache) getSortedTrackingKeys() []string {
+	//first we will build a slice
+	keys := make([]string, 0, len(cache.items))
+
+	for key := range cache.tracking {
+		keys = append(keys, key)
+	}
+
+	//next we must order this slice by the hits of its keys
+	sort.Slice(keys, func(i, j int) bool {
+		return cache.tracking[keys[i]].hits > cache.tracking[keys[j]].hits
+	})
+
+	return keys
+}
+
+// calcHeap will store the HeapAlloc bytes at cache.heap
 func (cache *Cache) calcHeap() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
--- lfu/cache_test.go
+++ lfu/cache_test.go
@@ -1,11 +1,15 @@
 package lfu
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
-	myStore := Init(5)
+	// No need to check whether `recover()` is nil. Just turn off the panic to can later test a wrong min size.
+	defer func() { recover() }()
+
+	myStore := Init(500000)
 
 	isCache := func(t interface{}) bool {
 		switch t.(type) {
@@ -20,9 +24,13 @@ func TestInit(t *testing.T) {
 		t.Fatalf("wrong init type")
 	}
 
-	if myStore.size != 5 {
+	if myStore.size != 500000 {
 		t.Fatalf("wrong cache size %d", myStore.size)
 	}
+
+	//we ware testing the panic: when code after Init() can be reached, something went wrong, so we manually quit with t.Fatalf()
+	Init(5)
+	t.Fatalf("there is a minimum of 500000")
 }
 
 func TestPutITem(t *testing.T) {
@@ -117,6 +125,45 @@ func TestForgetITem(t *testing.T) {
 
 	if err != nil {
 		t.Fatalf("%v", err)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	LFU.Put("foo", "bar")
+	LFU.Put("foo1", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+	LFU.Put("foo2", "bar ist aber nicht immer das beste")
+
+	max := LFU.heap
+
+	LFU.Put("foo3", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+	LFU.Put("foo4", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+	LFU.Put("foo5", "bar ist aber nicht immer das beste")
+	LFU.Put("foo6", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+
+	LFU.tracking["foo"] = Tracking{hits: 200, heap: 16}
+	LFU.tracking["foo1"] = Tracking{hits: 21, heap: 16}
+	LFU.tracking["foo2"] = Tracking{hits: 2000, heap: 16}
+	LFU.tracking["foo3"] = Tracking{hits: 21, heap: 16}
+	LFU.tracking["foo4"] = Tracking{hits: 23, heap: 16}
+	LFU.tracking["foo5"] = Tracking{hits: 2, heap: 16}
+	LFU.tracking["foo6"] = Tracking{hits: 18, heap: 16}
+
+	keys := LFU.getSortedTrackingKeys()
+	awaited := []string{"foo2", "foo", "foo4", "foo1", "foo3", "foo6", "foo5"}
+
+	if !reflect.DeepEqual(keys, awaited) {
+		t.Fatalf("not well ordered %v, awaited %v", keys, awaited)
+	}
+
+	//three items must be still available
+	LFU.reduce(max)
+
+	if LFU.heap >= max {
+		t.Fatalf("not correct reducing: %d, max: %d", LFU.heap, max)
+	}
+
+	if len(LFU.items) == 7 {
+		t.Fatal("no items reduced")
 	}
 }
 
