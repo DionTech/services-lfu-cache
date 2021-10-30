e3bb61d95d2fcfd26810aeef2ea6ea82967a145a
Daniel Koch <daniel.koch@publicare.de>
2021-10-30 20:51:20 +0200

    preparing heap allocation calc

--- lfu/cache.go
+++ lfu/cache.go
@@ -2,33 +2,59 @@ package lfu
 
 import (
 	"errors"
+	"runtime"
 	"sync"
 )
 
+type Tracking struct {
+	heap uint64
+	hits uint
+}
+
 type Cache struct {
-	size     uint
+	size     uint64
 	items    map[string]interface{}
-	tracking map[string]uint
+	tracking map[string]Tracking
 	lock     sync.RWMutex
+	heap     uint64
 }
 
 var LFU Cache
 
-func Init(size uint) Cache {
+//Init will init a new cache. Define the max size of the cache in bytes
+func Init(size uint64) Cache {
 	return Cache{size: size}
 }
 
 func (cache *Cache) Put(key string, item interface{}) (bool, error) {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+
 	if cache.items == nil {
 		cache.items = make(map[string]interface{})
 	}
 
+	if cache.tracking == nil {
+		cache.tracking = map[string]Tracking{}
+	}
+
+	actualHeap := cache.heap
+
 	cache.items[key] = item
+
+	cache.calcHeap()
+
+	itemHeap := cache.heap - actualHeap
+
+	cache.tracking[key] = Tracking{heap: itemHeap}
 
 	return true, nil
 }
 
 func (cache *Cache) Get(key string) (interface{}, error) {
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
+
 	item, exists := cache.items[key]
 
 	if !exists {
@@ -39,6 +65,9 @@ func (cache *Cache) Get(key string) (interface{}, error) {
 }
 
 func (cache *Cache) Forget(key string) error {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+
 	_, exists := cache.items[key]
 
 	//true means: we do not need to delete, but is the same as when deletion was successful
@@ -48,5 +77,14 @@ func (cache *Cache) Forget(key string) error {
 
 	delete(cache.items, key)
 
+	cache.calcHeap()
+
 	return nil
+}
+
+func (cache *Cache) calcHeap() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	cache.heap = m.HeapAlloc
 }
--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "lfu-cache/lfu"
+import (
+	"fmt"
+	"lfu-cache/lfu"
+	"runtime"
+	"strconv"
+	"time"
+)
 
 func main() {
 	lfu.LFU = lfu.Init(20)
+
+	PrintMemUsage()
+
+	lfu.LFU.Put("foo", "bar")
+
+	PrintMemUsage()
+
+	lfu.LFU.Put("foo2", []string{"foo", "bar", "irgendwas"})
+	//var m1, m2 runtime.MemStats
+	for i := 0; i < 100; i++ {
+
+		// Allocate memory using make() and append to overall (so it doesn't get
+		// garbage collected). This is to create an ever increasing memory usage
+		// which we can track. We're just using []int as an example.
+		//runtime.ReadMemStats(&m1)
+		lfu.LFU.Put(strconv.Itoa(i), i)
+		//runtime.ReadMemStats(&m2)
+		//memUsage(&m1, &m2)
+
+		// Print our memory usage at each interval
+		PrintMemUsage()
+		time.Sleep(time.Second)
+	}
+
+	PrintMemUsage()
+
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", m.Alloc)
+	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc)
+	fmt.Printf("\tHEAPAlloc = %v MiB", m.HeapAlloc)
+	fmt.Printf("\tSys = %v MiB", m.Sys)
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	fmt.Printf("\tMCacheSys = %v\n", m.MCacheSys)
+	/*
+		fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+		fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+		fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+		fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	*/
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func memUsage(m1, m2 *runtime.MemStats) {
+	fmt.Println("Alloc:", m2.Alloc-m1.Alloc,
+		"TotalAlloc:", m2.TotalAlloc-m1.TotalAlloc,
+		"HeapAlloc:", m2.HeapAlloc-m1.HeapAlloc)
 }
